0d90c3e1b9cfaac6d1bd730a1fbe03ed596c7563
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-19 21:25:34 +0530

    Setup initial app router or pages for the app

---
+++ app/(dashboard)/jobs/page.tsx
@@ -0,0 +1,5 @@
+const JobsPage = () => {
+  return <div>JobsPage</div>;
+};
+
+export default JobsPage;
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default layout;
